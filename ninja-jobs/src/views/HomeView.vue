a26e6fe6dfeafe324cfd31f9ed942016699742ce
AdaN6 <a_disney_da@hotmail.com>
2023-03-28 18:53:32 +0200

    edited home and about page

--- ninja-jobs/src/views/HomeView.vue
+++ ninja-jobs/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nulla dolorem quos recusandae reprehenderit facere vero magni delectus praesentium! Quisquam, repudiandae. Quia vitae, nulla similique saepe eveniet ea repellendus voluptatum animi praesentium, recusandae excepturi dolorem facere error quod minus culpa placeat qui assumenda sapiente corrupti voluptas blanditiis optio. Reiciendis dolore, ad repellat excepturi ut minima, quos quo sequi dolorem rerum totam culpa, dolores voluptatem. Necessitatibus accusantium quasi aut doloribus voluptate ipsam nihil veniam, provident pariatur voluptatem earum quam dolore labore hic non, laboriosam eligendi eos eum vitae reiciendis culpa. Ullam facilis sunt, atque repellendus explicabo magni voluptas quo dolorum alias?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nulla dolorem quos recusandae reprehenderit facere vero magni delectus praesentium! Quisquam, repudiandae. Quia vitae, nulla similique saepe eveniet ea repellendus voluptatum animi praesentium, recusandae excepturi dolorem facere error quod minus culpa placeat qui assumenda sapiente corrupti voluptas blanditiis optio. Reiciendis dolore, ad repellat excepturi ut minima, quos quo sequi dolorem rerum totam culpa, dolores voluptatem. Necessitatibus accusantium quasi aut doloribus voluptate ipsam nihil veniam, provident pariatur voluptatem earum quam dolore labore hic non, laboriosam eligendi eos eum vitae reiciendis culpa. Ullam facilis sunt, atque repellendus explicabo magni voluptas quo dolorum alias?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates nulla dolorem quos recusandae reprehenderit facere vero magni delectus praesentium! Quisquam, repudiandae. Quia vitae, nulla similique saepe eveniet ea repellendus voluptatum animi praesentium, recusandae excepturi dolorem facere error quod minus culpa placeat qui assumenda sapiente corrupti voluptas blanditiis optio. Reiciendis dolore, ad repellat excepturi ut minima, quos quo sequi dolorem rerum totam culpa, dolores voluptatem. Necessitatibus accusantium quasi aut doloribus voluptate ipsam nihil veniam, provident pariatur voluptatem earum quam dolore labore hic non, laboriosam eligendi eos eum vitae reiciendis culpa. Ullam facilis sunt, atque repellendus explicabo magni voluptas quo dolorum alias?</p>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
