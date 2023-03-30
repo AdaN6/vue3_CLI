6a80a8efb43cee80548038eed1f8dda3ced130c0
AdaN6 <a_disney_da@hotmail.com>
2023-03-30 14:06:03 +0200

    Composition Api basics

---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+
+    <h2>Dojo blog</h2>
+    <h3>{{ name }} {{ age }}</h3>
+    <button @click="handleClick">Click</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+
+// setup function will run before anything else
+// inside setup can write any normal js or create a computed property
+ setup() {
+ console.log('setup')
+
+ let name = 'mario'
+ let age = 30
+
+ // create function
+ const handleClick = () => {
+  console.log('clicked!')
+ }
+
+ return {name, age, handleClick}
+},
+
+}
+</script>
