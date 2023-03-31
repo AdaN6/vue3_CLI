f7484f075b6b0dc5fb63daaea342428c3cc6b701
AdaN6 <a_disney_da@hotmail.com>
2023-03-31 23:57:52 +0200

    template ref using in setup function

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
 
     <h2>Dojo blog</h2>
-    <h3>{{ name }} {{ age }}</h3>
+    <h3 ref="p">{{ name }} {{ age }}</h3>
     <button @click="handleClick">Click</button>
   </div>
 </template>
@@ -10,23 +10,31 @@
 <script>
 // @ is an alias to /src
 
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
 
 // setup function will run before anything else
 // inside setup can write any normal js or create a computed property
- setup() {
- console.log('setup')
+setup() {
+  console.log('setup')
+  // ref will be using differently in setup function as it cant use this.$ref as we always do in other life cycle hook like mounted or method... 
+  //it has to import ref from vue when using ref
+
+  const p = ref(null)
 
  let name = 'mario'
  let age = 30
 
  // create function
  const handleClick = () => {
-  console.log('clicked!')
+  console.log(p, p.value)
+  p.value.classList.add('test')
+  p.value.textContent = 'Hello, hey'
  }
 
- return {name, age, handleClick}
+ return {name, age, handleClick, p}
 },
 
 }
