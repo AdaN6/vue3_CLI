f1fa9bbfab4a9b6a5c7bafe46a0476ebad880abd
AdaN6 <a_disney_da@hotmail.com>
2023-04-02 23:46:30 +0200

    Lifecycle hook in setup function v1: mount and unmount posts

--- dojo-blog-2/src/components/PostList.vue
+++ dojo-blog-2/src/components/PostList.vue
@@ -8,13 +8,24 @@
 </template>
 
 <script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
 import SinglePost from './SinglePost.vue'
 
 export default {
   props: ['posts'],
   components: { SinglePost }, 
   setup(props) {
-    console.log(props.posts)
+  
+    // lifecycle hook in setup function
+    onMounted(() => {
+      console.log('component mounted')
+    })
+    onUnmounted(() => {
+      console.log('component unmouted')
+    })
+    onUpdated(() => {
+      console.log('component updated')
+    })
   }
 }
 </script>
