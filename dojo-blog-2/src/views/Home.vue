61d71a7d5a67cd824b9ba65f1247ff70327353e8
AdaN6 <a_disney_da@hotmail.com>
2023-04-03 00:16:22 +0200

    created async funtion, fetched data, and created thorw and catch error

--- dojo-blog-2/src/views/Home.vue
+++ dojo-blog-2/src/views/Home.vue
@@ -19,6 +19,23 @@ export default {
       { title: 'welcome to the blog', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.', id: 1 },
       { title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 },
     ]) 
+
+    const posts2 = ref([])
+    const error = ref(null)
+
+    const load = async () => {
+      try{
+        let data = await fetch('http://localhost:3000/posts')
+        console.log(data)
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+      } catch(err){
+        error.value = err.message
+      }
+    }
+
+    load()
   
   const showPosts = ref(true)
 
