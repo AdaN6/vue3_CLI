3ae29c84982ca9c8fcb4e4b9ab1ad1603b604a38
AdaN6 <a_disney_da@hotmail.com>
2023-04-02 20:03:47 +0200

    ref - reactive value

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -2,8 +2,12 @@
   <div class="home">
 
     <h2>Dojo blog</h2>
-    <h3 ref="p">{{ name }} {{ age }}</h3>
+    <!-- RT -->
+    <!-- <h3 ref="p">{{ name }} {{ age }}</h3> -->
+    <h3> My name is {{ name }} and I am {{ age }} years old</h3>
     <button @click="handleClick">Click</button>
+    <input type="text" v-model="name">
+    <button @click="age++">Increase Age</button>
   </div>
 </template>
 
@@ -22,19 +26,29 @@ setup() {
   // ref will be using differently in setup function as it cant use this.$ref as we always do in other life cycle hook like mounted or method... 
   //it has to import ref from vue when using ref
 
-  const p = ref(null)
+  // --> Template ref (RT)
+  //   const p = ref(null)
 
- let name = 'mario'
- let age = 30
+  //  let name = 'mario'
+  //  let age = 30
+
+  // --> refs for reactivity (RR)
+  const name = ref('mario')
+  const age = ref(30)
+
 
  // create function
  const handleClick = () => {
-  console.log(p, p.value)
-  p.value.classList.add('test')
-  p.value.textContent = 'Hello, hey'
+  // ---> RT
+  // console.log(p, p.value)
+  // p.value.classList.add('test')
+  // p.value.textContent = 'Hello, hey'
+  // ----> RR
+  name.value = 'luigi'
+  age.value = 40
  }
 
- return {name, age, handleClick, p}
+ return {name, age, handleClick}
 },
 
 }
