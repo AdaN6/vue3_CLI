7b1c3de1a087f66bf1c92e5f554fa5c4e0a1746a
AdaN6 <a_disney_da@hotmail.com>
2023-04-03 17:34:09 +0200

    created a getPost as composable file and loaded in Details view

--- dojo-blog-2/src/views/Details.vue
+++ dojo-blog-2/src/views/Details.vue
@@ -1,9 +1,29 @@
 <template>
-  details
+    <div v-if="error">
+        {{ error }}
+    </div>
+    <div v-if="post" class="post">
+        <h3>
+            {{ post.title }}
+        </h3>
+        <p class="pre">
+            {{ post.body  }}
+        </p>
+    </div>
 </template>
 
 <script>
+import getPost from '../composables/getPost.js'; 
+
 export default {
+    props: ['id'],
+    setup(props) {
+        const {post, error, load} = getPost(props.id)
+
+        load()
+
+        return {post, error}
+    }
 
 }
 </script>
