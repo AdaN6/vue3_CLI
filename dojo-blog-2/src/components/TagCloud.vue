f12d81a15a363d2cac10197b74cc55a13e11224b
AdaN6 <a_disney_da@hotmail.com>
2023-04-17 20:48:41 +0200

    created tag cloud list and router-link to tag post

--- dojo-blog-2/src/components/TagCloud.vue
+++ dojo-blog-2/src/components/TagCloud.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="tag-cloud">
     <h3>Tags</h3>
-    <div>
-        
+    <div v-for="tag in tags" :key="tag">
+        <router-link :to="{name: 'Tag', params:{tag}}">
+            #{{ tag }}
+        </router-link>
     </div>
   </div>
 </template>
 
 <script>
+import useTags from '@/composables/useTags';
+
 export default {
+    props: ['posts2'],
+    setup(props) {
+        const { tags } = useTags(props.posts2)
+
+        return {tags}
+    }
 
 }
 </script>
