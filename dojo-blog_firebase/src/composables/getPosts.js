097b9ea38f3ee586a5e1aab014307c4ef1e84b53
AdaN6 <a_disney_da@hotmail.com>
2023-04-19 12:07:14 +0200

    applied the logic of realtime update in getPosts

--- dojo-blog_firebase/src/composables/getPosts.js
+++ dojo-blog_firebase/src/composables/getPosts.js
@@ -14,14 +14,21 @@ const getPosts = () => {
             
             const res = await projectFirestore.collection('posts')
             .orderBy('createAt', 'desc')
-            .get()           
+            // .get()           
             // console.log(res.docs)
+            .onSnapshot((snap) => {
+                // console.log(snap)
+                let docs = snap.docs.map(doc => {
+                    return { ...doc.data(), id: doc.id}
 
-            posts.value = res.docs.map(doc => {
-                // console.log(doc.data())
+                })
+                posts.value = docs
 
-                // as the id is on the data not in the data and we needed the document id. ...doc.data() = grab all the properties into "doc.data()" object. and then add 2nd property to include the id
-                return { ...doc.data(), id: doc.id}
+            // posts.value = res.docs.map(doc => {
+            //     // console.log(doc.data())
+
+            //     // as the id is on the data not in the data and we needed the document id. ...doc.data() = grab all the properties into "doc.data()" object. and then add 2nd property to include the id
+            //     return { ...doc.data(), id: doc.id}
             })
             
         } catch(err){
